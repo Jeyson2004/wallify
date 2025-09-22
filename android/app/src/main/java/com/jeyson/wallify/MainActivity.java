6db8806f0b691ba97adc1f78a59d66e55a1dc585
JeysonTorres <jeyson2004@outlook.es>
2025-09-22 03:47:15 -0500

    first commit

---
+++ android/app/src/main/java/com/jeyson/wallify/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jeyson.wallify;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jeyson.wallify',
+  appName: 'My_wallpaper_app',
+  webDir: 'www'
+};
+
+export default config;
